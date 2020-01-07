139e96ab08cc2fc1f0b7364d449ccb39ce398c40
RASHANPHILIPP <50788170+RASHANZ@users.noreply.github.com>
2020-01-07 17:50:39 +0530

    Add files via upload

---
+++ PHILIPP.sh
@@ -0,0 +1,8 @@
+
+toilet -f future CODED BY RASHANPHILIPP|lolcat
+sleep 2
+clear
+
+toilet -f future INSTALL-MSF | lolcat
+sleep 2
+curl -LO https://github.com/termux/termux-packages/files/3995119/metasploit_5.0.65-1_all.deb.gz && gunzip metasploit_5.0.65-1_all.deb.gz && dpkg -i metasploit_5.0.65-1_all.deb
