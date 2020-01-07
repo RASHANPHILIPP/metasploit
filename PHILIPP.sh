1d3d490568a0d6172030febdad9ba5a29de7beb8
RASHANPHILIPP <50788170+RASHANZ@users.noreply.github.com>
2020-01-07 18:09:38 +0530

    Update PHILIPP.sh

--- PHILIPP.sh
+++ PHILIPP.sh
@@ -1,8 +1,9 @@
 command pkg install toilet && pkg install ruby && gem install lolcat
+
 toilet -f future CODED BY RASHANPHILIPP|lolcat
 sleep 2
 clear
 
-toilet -f future INSTALLING-MSF WAIT TILL COMPLETE| lolcat
+toilet -f future INSTALLING-MSF WAIT| lolcat
 sleep 2
 curl -LO https://github.com/termux/termux-packages/files/3995119/metasploit_5.0.65-1_all.deb.gz && gunzip metasploit_5.0.65-1_all.deb.gz && dpkg -i metasploit_5.0.65-1_all.deb
