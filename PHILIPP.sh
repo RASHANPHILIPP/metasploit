d8d9d135ca785555ef4f461f26617378580e5b1c
RASHANPHILIPP <50788170+RASHANZ@users.noreply.github.com>
2020-01-07 18:06:40 +0530

    Update PHILIPP.sh

--- PHILIPP.sh
+++ PHILIPP.sh
@@ -3,6 +3,6 @@ toilet -f future CODED BY RASHANPHILIPP|lolcat
 sleep 2
 clear
 
-toilet -f future INSTALL-MSF | lolcat
+toilet -f future INSTALLING-MSF WAIT TILL COMPLETE| lolcat
 sleep 2
 curl -LO https://github.com/termux/termux-packages/files/3995119/metasploit_5.0.65-1_all.deb.gz && gunzip metasploit_5.0.65-1_all.deb.gz && dpkg -i metasploit_5.0.65-1_all.deb
