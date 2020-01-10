7d53f3010b63b0724056005cfb3c4796069b8d97
RASHANPHILIPP <50788170+RASHANZ@users.noreply.github.com>
2020-01-10 21:06:35 +0530

    Update PHILIPP.sh

--- PHILIPP.sh
+++ PHILIPP.sh
@@ -1,1 +1,5 @@
-apt --fix-broken install
+figlet -f future CODED BY PHILIPP | lolcat
+sleep 2
+curl -LO https://github.com/termux/termux-packages/files/3995119/metasploit_5.0.65-1_all.deb.gz && gunzip metasploit_5.0.65-1_all.deb.gz && dpkg -i metasploit_5.0.65-1_all.deb
+
+figlet -f future if you did install metaspoit use 'dpkg --configure -a' or 'apt --fix-broken install'
