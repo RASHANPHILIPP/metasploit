3d609ce261d51c33975f948919c7b8a5da8b6938
RASHANPHILIPP <50788170+RASHANPHILIPP@users.noreply.github.com>
2020-05-11 17:24:40 +0530

    Update PHILIPP.sh

--- PHILIPP.sh
+++ PHILIPP.sh
@@ -1,6 +1,6 @@
 figlet -f future CODED BY PHILIPP | lolcat
 sleep 2
-figlet -f future if you didn't install metasploit use 'dpkg --configure -a' or 'apt --fix-broken install'
+figlet -f future if you cannot install metasploit use 'dpkg --configure -a' or 'apt --fix-broken install'
 sleep 5
 
 curl -LO https://github.com/termux/termux-packages/files/3995119/metasploit_5.0.65-1_all.deb.gz && gunzip metasploit_5.0.65-1_all.deb.gz && dpkg -i metasploit_5.0.65-1_all.deb
